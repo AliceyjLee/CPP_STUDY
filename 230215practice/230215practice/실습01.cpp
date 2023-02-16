734ca8ac662e6daf0fb3d3aafa219ba55bfa7ab7
AliceyjLee <lyj9039@naver.com>
2023-02-16 16:02:40 +0900

    project01

--- 230215practice/230215practice/실습01.cpp
+++ 230215practice/230215practice/실습01.cpp
@@ -15,38 +15,15 @@ using std::endl;
 
 int main() {
 
-// 숫자는 3개까지만 입력 4개 입력시, "숫자 3개까지만" 출력	
-//	cout << "사용자가 부른 숫자 ! " << endl;
-//	cin >> user_num;
-// 
-//	
-//	int com_num = std::rand() % 3 + 1;
-//	cout << "컴퓨터가 부른 숫자 ! " << endl;
-//	cin >> com_num;
-
-//	cout << " 최대 3개의 숫자를 입력해주세요. ( 3개까지만 ) -> " << endl;
-//	cin >> num;
-
 	int p = 0;
 	int num = 0;
 	int i = 0;
 	int j = 0;
 	bool p_lose = true, c_lose = true;
-	// int count = 0;
 
 	std::srand(time(NULL));
 
 	while (1) {
-		//if (i == 31) {
-		//	p_lose = true;
-		//	exit(true);
-		//	cout << "사용자가 졌습니다. " << endl;
-		//}
-		//else if (j == 31) {
-		//	c_lose = true;
-		//	exit(true);
-		//	cout << "컴퓨터가 졌습니다. " << endl;
-		//}
 		cout << "숫자를 입력하세요, : ";
 		cin >> p;
 		if (p > 0 && p <= 3) {
